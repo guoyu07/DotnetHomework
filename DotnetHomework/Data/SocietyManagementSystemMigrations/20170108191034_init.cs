37ec324a1a7488c02e1364dba9ee65a156f7924a
czp <czp3009@gmail.com>
2017-01-09 03:11:09 +0800

    生成 Migrations

---
+++ DotnetHomework/Data/SocietyManagementSystemMigrations/20170108191034_init.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotnetHomework.Data.SocietyManagementSystemMigrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    CreateTime = table.Column<DateTime>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Reason = table.Column<string>(nullable: true),
+                    Society = table.Column<int>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    Time = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Member",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    EntryPost = table.Column<string>(nullable: true),
+                    EntryTime = table.Column<DateTime>(nullable: true),
+                    Society = table.Column<int>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    User = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Member", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SocietyCategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocietyCategory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Society",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Category = table.Column<int>(nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: true),
+                    Creator = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Reason = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Society", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TakePart",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Activity = table.Column<int>(nullable: false),
+                    Time = table.Column<DateTime>(nullable: true),
+                    User = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TakePart", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VActivityInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    CreateTime = table.Column<DateTime>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Reason = table.Column<string>(nullable: true),
+                    SocietyDescription = table.Column<string>(nullable: true),
+                    SocietyId = table.Column<int>(nullable: false),
+                    SocietyName = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    Time = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VActivityInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VMemberInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    EntryPost = table.Column<string>(nullable: true),
+                    EntryTime = table.Column<DateTime>(nullable: true),
+                    SocietyDescription = table.Column<string>(nullable: true),
+                    SocietyId = table.Column<int>(nullable: false),
+                    SocietyName = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VMemberInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VSocietyInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    CategoryId = table.Column<int>(nullable: false),
+                    CategoryName = table.Column<string>(nullable: true),
+                    CreateTime = table.Column<DateTime>(nullable: true),
+                    CreatorId = table.Column<string>(nullable: true),
+                    CreatorName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    MemberCount = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Reason = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VSocietyInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VTakePartInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    ActivityDescription = table.Column<string>(nullable: true),
+                    ActivityId = table.Column<int>(nullable: false),
+                    ActivityName = table.Column<string>(nullable: true),
+                    ActivityStatus = table.Column<string>(nullable: true),
+                    ActivityTime = table.Column<DateTime>(nullable: true),
+                    ApplicationPeriod = table.Column<DateTime>(nullable: true),
+                    SocietyId = table.Column<int>(nullable: false),
+                    SocietyName = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VTakePartInfo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Activity");
+
+            migrationBuilder.DropTable(
+                name: "Member");
+
+            migrationBuilder.DropTable(
+                name: "SocietyCategory");
+
+            migrationBuilder.DropTable(
+                name: "Society");
+
+            migrationBuilder.DropTable(
+                name: "TakePart");
+
+            migrationBuilder.DropTable(
+                name: "VActivityInfo");
+
+            migrationBuilder.DropTable(
+                name: "VMemberInfo");
+
+            migrationBuilder.DropTable(
+                name: "VSocietyInfo");
+
+            migrationBuilder.DropTable(
+                name: "VTakePartInfo");
+        }
+    }
+}
---
+++ DotnetHomework/Data/SocietyManagementSystemMigrations/SocietyManagementSystemDbContextModelSnapshot.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using DotnetHomework.Data;
+
+namespace DotnetHomework.Data.SocietyManagementSystemMigrations
+{
+    [DbContext(typeof(SocietyManagementSystemDbContext))]
+    partial class SocietyManagementSystemDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1");
+
+            modelBuilder.Entity("DotnetHomework.Data.SocietyManagementSystemEntities.ActivityEntity", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime?>("CreateTime");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("Reason");
+
+                    b.Property<int>("Society");
+
+                    b.Property<string>("Status");
+
+                    b.Property<DateTime?>("Time");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Activity");
+                });
+
+            modelBuilder.Entity("DotnetHomework.Data.SocietyManagementSystemEntities.MemberEntity", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("EntryPost");
+
+                    b.Property<DateTime?>("EntryTime");
+
+                    b.Property<int>("Society");
+
+                    b.Property<string>("Status");
+
+                    b.Property<string>("User");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Member");
+                });
+
+            modelBuilder.Entity("DotnetHomework.Data.SocietyManagementSystemEntities.SocietyCategoryEntity", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("SocietyCategory");
+                });
+
+            modelBuilder.Entity("DotnetHomework.Data.SocietyManagementSystemEntities.SocietyEntity", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Category");
+
+                    b.Property<DateTime?>("CreateTime");
+
+                    b.Property<string>("Creator");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("Reason");
+
+                    b.Property<string>("Status");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Society");
+                });
+
+            modelBuilder.Entity("DotnetHomework.Data.SocietyManagementSystemEntities.TakePartEntity", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Activity");
+
+                    b.Property<DateTime?>("Time");
+
+                    b.Property<string>("User");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("TakePart");
+                });
+
+            modelBuilder.Entity("DotnetHomework.Data.SocietyManagementSystemEntities.VActivityInfoEntity", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime?>("CreateTime");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("Reason");
+
+                    b.Property<string>("SocietyDescription");
+
+                    b.Property<int>("SocietyId");
+
+                    b.Property<string>("SocietyName");
+
+                    b.Property<string>("Status");
+
+                    b.Property<DateTime?>("Time");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("VActivityInfo");
+                });
+
+            modelBuilder.Entity("DotnetHomework.Data.SocietyManagementSystemEntities.VMemberInfoEntity", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("EntryPost");
+
+                    b.Property<DateTime?>("EntryTime");
+
+                    b.Property<string>("SocietyDescription");
+
+                    b.Property<int>("SocietyId");
+
+                    b.Property<string>("SocietyName");
+
+                    b.Property<string>("Status");
+
+                    b.Property<string>("UserId");
+
+                    b.Property<string>("UserName");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("VMemberInfo");
+                });
+
+            modelBuilder.Entity("DotnetHomework.Data.SocietyManagementSystemEntities.VSocietyInfoEntity", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("CategoryId");
+
+                    b.Property<string>("CategoryName");
+
+                    b.Property<DateTime?>("CreateTime");
+
+                    b.Property<string>("CreatorId");
+
+                    b.Property<string>("CreatorName");
+
+                    b.Property<string>("Description");
+
+                    b.Property<int>("MemberCount");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("Reason");
+
+                    b.Property<string>("Status");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("VSocietyInfo");
+                });
+
+            modelBuilder.Entity("DotnetHomework.Data.SocietyManagementSystemEntities.VTakePartInfoEntity", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ActivityDescription");
+
+                    b.Property<int>("ActivityId");
+
+                    b.Property<string>("ActivityName");
+
+                    b.Property<string>("ActivityStatus");
+
+                    b.Property<DateTime?>("ActivityTime");
+
+                    b.Property<DateTime?>("ApplicationPeriod");
+
+                    b.Property<int>("SocietyId");
+
+                    b.Property<string>("SocietyName");
+
+                    b.Property<string>("UserId");
+
+                    b.Property<string>("Username");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("VTakePartInfo");
+                });
+        }
+    }
+}
